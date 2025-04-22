af53059c85707b68c2dd6c41da1068a3b578af23
hjtejuco123123 <hjtejuco@feutech.edu.ph>
2025-04-22 13:59:10 +0800

    up

--- module1/HelloWorld.java
+++ module1/HelloWorld.java
@@ -1,9 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        int sum = a + b;
-        System.out.println("Sum: " + sum);
+        System.err.println("Hello World");
      
     }   
 }
--- module1/sample.py
+++ module1/sample.py
@@ -1,2 +1,13 @@
-print ("hello World")
-print ("hello world again")
+'''
+    This is a multi line comment
+'''
+
+# print ("hello World")     #this is a comment
+# print ("hello world again")
+
+#indentation
+
+# if 5 > 2:
+#     print ("five is greater than two") #this is a comment
+#     print ("this block of code belongs to the if block")
+# print ("this is outside of the if block statememt")
