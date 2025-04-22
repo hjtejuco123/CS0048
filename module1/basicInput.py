e92fe911df013ee21329b91d68819d4d16f0f2fa
hjtejuco123123 <hjtejuco@feutech.edu.ph>
2025-04-22 14:49:12 +0800

    up

--- module1/basicInput.py
+++ module1/basicInput.py
@@ -1,6 +1,10 @@
 #User Input
 
-name = input("Enter your name ")
-print (f"Hello, {name}!")
+# name = input("Enter your name ")
+# print (f"Hello, {name}!")
 
+#convert str into int
+
+# age = int(input ("Enter you age "))
+# print (f"Next you will be  {age+1} years old.")
 
