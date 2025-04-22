4463c0aba1154f49ad5ca1384877c943f7f5b92b
hjtejuco123123 <hjtejuco@feutech.edu.ph>
2025-04-22 15:00:16 +0800

    up

--- module1/basicInput.py
+++ module1/basicInput.py
@@ -15,7 +15,21 @@
 
 
 #menu selection
+#\n = new line
 
+print ("1. Option A \n2. Option B\n3. Option C")
+choice = input("Select option from 1-3: ")
+if choice == '1':
+    print ("You have selected A")
+elif choice == '2':
+    print ("You have selected B")
+elif choice == '3':
+    print ("You have selected C")
+else:
+    print ("invalid choice")
+
+
+    
 
 
 
