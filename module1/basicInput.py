34e5ca922049416f721cee2c418acf748d29d659
hjtejuco123123 <hjtejuco@feutech.edu.ph>
2025-04-22 14:54:42 +0800

    up

--- module1/basicInput.py
+++ module1/basicInput.py
@@ -8,3 +8,16 @@
 # age = int(input ("Enter you age "))
 # print (f"Next you will be  {age+1} years old.")
 
+#multiple inputs
+
+# x,y = input("Enter two numbers separated by space ").split()
+# print (f"Sum {int(x) + int(y)}")
+
+
+#menu selection
+
+
+
+
+
+
