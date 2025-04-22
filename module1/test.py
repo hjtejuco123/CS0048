aa643c8a4b26db4d810cc0e6fd0e4a79e0cbaccc
hjtejuco123123 <hjtejuco@feutech.edu.ph>
2025-04-22 08:46:28 +0800

    up

--- module1/test.py
+++ module1/test.py
@@ -1,1 +1,13 @@
-print ("hello world")
+#if statement (indentation)
+# if 5 > 2:
+#     print ("Five is greater than two")
+#     print ("this is still part of the if block")
+# print ("this is outside of the if block")
+
+def greet(name):
+    print (f"hello, {name}!")
+    if len(name)>5:
+        print ("This is a long name")
+
+
+greet ("Joanss")
