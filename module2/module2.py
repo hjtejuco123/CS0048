45d2600f9f5bcb02171c8513ccef643512f12a12
hjtejuco123123 <hjtejuco@feutech.edu.ph>
2025-04-29 07:28:05 +0800

    up

---
+++ module2/module2.py
@@ -0,0 +1,15 @@
+# Check for True
+# Check for False
+
+#  Non-Zero and Non-Null values are True
+#  Zero and Null are False
+
+'''
+if condition:
+    # block of codes
+'''
+
+num = 5
+if num > 0:
+    print ("Positive Number")
+print ("Continue with program code ...")
