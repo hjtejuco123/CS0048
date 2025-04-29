7f942a5a99a17256944021d74d737aacb8315cf8
hjtejuco123123 <hjtejuco@feutech.edu.ph>
2025-04-29 08:34:09 +0800

    up

---
+++ module2/sample1.py
@@ -0,0 +1,17 @@
+'''
+Banking System
+
+Menu
+    Deposit
+    Withdraw
+    Check Balance
+    Exit
+
+Logical Flow
+1. Start with balance = 0
+2. Show menu
+3. Ask user for choice
+4. Perform the operation (menu 1-4)
+5. Loop is terminated if the user selects 4
+'''
+
