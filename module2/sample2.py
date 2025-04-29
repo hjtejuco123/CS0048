8c032e278f586e061ecf275a358eb79c92654ee7
hjtejuco123123 <hjtejuco@feutech.edu.ph>
2025-04-29 09:02:52 +0800

    up

---
+++ module2/sample2.py
@@ -0,0 +1,21 @@
+'''
+ATM Pin System
+
+Problem Details
+
+User must input the corrorect pin to access the services
+allowed 3 times
+
+Services includes
+
+1. Check Balance
+2. Withdraw
+3. Exit
+
+Logical Flow
+
+1. Set correct PIN
+2. Ask user to input PIN
+3. Allowed max 3 times
+4. If successful show the menu
+'''
