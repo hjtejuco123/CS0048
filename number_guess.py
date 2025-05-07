5496cdf4eff6176164d9a7074cd77ff04b3cf4bd
Ken  Cedric R. Marzan <128457366+KenMarzan@users.noreply.github.com>
2025-05-07 09:54:41 +0800

    Add files via upload
    Student average to do

---
+++ number_guess.py
@@ -0,0 +1,50 @@
+import random 
+
+def number_guess():
+    num = random.randint(0,100)
+    attemps = 0
+
+    while True:
+        print("==========================")
+        guess = int(input("Guess the number: "))
+        if num == guess:
+            print("You are correct!")
+            attemps += 1
+            print(f"You've guess the number in: ", attemps, "attempt(s)")
+
+            choice = input("Do you want to continue?: ")
+            if choice == "Y" or "y":
+                continue
+            elif choice == "N" or "n":
+                break
+            else: 
+                print("Wrong choice")
+        elif guess < num:
+            print("You are less than the given num")
+            attemps += 1
+
+        elif guess > num:
+            print("You are greater than the given num")
+            attemps += 1
+        else:
+           print("Wrong choice")
+           break
+while True:
+    print("======================")
+    print(f"a. Play Number Guessing Game")
+    print(f"b. Exit")
+   
+    choice = input("Enter your choice:")
+   
+    if choice == "a":
+      number_guess()
+    elif choice == "b":
+        print(f"Thank you for using my program!")
+        break
+    else:
+        print(f"Invalid choice")
+
+
+    
+
+
