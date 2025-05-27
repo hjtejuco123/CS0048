fe791ea1a7d71874f37c807170c09f22a367b304
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-05-27 14:01:40 +0800

    Update sample.py

--- module4/sample.py
+++ module4/sample.py
@@ -50,5 +50,51 @@ print (f"{student1.name} scored {student1.grade}% in the certification")
 
 ####################################################################
 
+#student 
+#student -> name, age, grade level
+#student -> study, can take exam 
+
+class Student:
+    #constructor
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade 
+    #method    
+    def study(self):
+        return f"{self.name} is studing for {self.grade} grade."
+    
+    #method
+    def take_exam(self):
+        return f"{self.name} is taking an exam. "
+ 
+class HonorsStudent(Student):
+    #constructor
+    def __init__(self, name, age, grade, scholarship_amout):
+        super().__init__(name, age, grade)
+        self.scholarship_amout = scholarship_amout
+    
+    #Override    
+    def study(self):
+        return f"{self.name} is studing intensely for {self.grade} grade honor classes."
+    
+    #method
+    def recieve_scholarship(self):
+        return f"{self.name} recieve a ${self.scholarship_amout} scholarship. "
+ 
+ 
+#usage 
+
+regular_studennt = Student("Hadji", 12, "6th")
+honors_student = HonorsStudent("Joan", 13, "8th",1000)
+
+print (regular_studennt.study())
+print (honors_student.study()) 
+print (honors_student.take_exam())    
+print (honors_student.recieve_scholarship()) 
+
+####################################################################
+
+
 
 
