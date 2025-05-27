cd7478119f8b6b3cd5c52adf398ef07f0296d574
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-05-27 14:47:14 +0800

    Update sample.py

--- module4/sample.py
+++ module4/sample.py
@@ -187,7 +187,42 @@ if __name__ == "__main__":
     main()
             
 ####################################################################
+#encapsulation
+
+class BankAccount:
+    #constructor
+    def __init__ (self, account_holder, initial_balance = 0):
+        self.account_holder = account_holder
+        self.__balance = initial_balance
+    
+    #deposit method 
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount 
+            return f"Deposited {amount}. New balance is {self.__balance}. "
+        return "Deposit amount must be positive "
+    
+    #withdraw
+    def withdraw(self, amount):
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount 
+            return f"Withdrew {amount}. New balance is {self.__balance}. "
+        return "Invalid withdrawal amount"
+
+    def get_balance(self):
+        return self.__balance
+    
+    def get_account_holder(self):
+        return self.account_holder
+    
+
+account = BankAccount ("Hadji",1000)
+print (account.deposit(500))
+print (account.withdraw(200))
+print (account.get_balance())
+print (account.account_holder)
 
 
+####################################################################
 
 
